1a37a51f1806d967e934da7ed566a6354ce71a82
eLBati <lb@takobi.online>
2019-11-04 12:51:03 +0100

    ADD account_invoice_create_product_from_line

---
+++ account_invoice_create_product_from_line/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2019 Lorenzo Battistini @ TAKOBI
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Create product from invoice line",
+    "description": "This module allows to create a new product from an invoice "
+                   "line, allowing user to set product data, without "
+                   "overwriting data already present in invoice line",
+    "version": "12.0.1.0.0",
+    "category": "Invoices & Payments",
+    "website": "https://takobi.online",
+    "author": "TAKOBI",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "wizard/create_product_view.xml",
+        "views/account_invoice_view.xml",
+    ],
+    "demo": [
+    ],
+}
---
+++ account_invoice_create_product_from_line/wizard/create_product.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api
+
+
+class CreateProduct(models.TransientModel):
+    _name = 'wizard.create.product.from.invoice.line'
+    _description = 'Create product from invoice line'
+
+    name = fields.Char("Name", required=True)
+    code = fields.Char("Code")
+    unit_price = fields.Float("Unit price")
+    uom_id = fields.Many2one('uom.uom', 'Unit of Measure', required=True)
+
+    @api.multi
+    def create_product(self):
+        self.ensure_one()
+        line_id = self.env.context.get('active_id')
+        inv_line = self.env['account.invoice.line'].browse(line_id)
+        product = self.env['product.product'].create({
+            'name': self.name,
+            'default_code': self.code,
+            'standard_price': self.unit_price,
+            'uom_id': self.uom_id.id,
+        })
+        inv_line.product_id = product.id
+
+    @api.model
+    def default_get(self, fields):
+        vals = super(CreateProduct, self).default_get(fields)
+        if not vals.get('uom_id'):
+            vals['uom_id'] = self.env['product.template']._get_default_uom_id()
+        return vals
